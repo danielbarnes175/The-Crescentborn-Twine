e459b95deff919e9c3313d3156f2b7d00f9ff66d
Daniel Barnes <danielbarnes175@gmail.com>
2025-08-03 19:08:58 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'no-new': 'off',
+    'no-undef': 'off',
+    'no-restricted-syntax': 'warn',
+    'no-plusplus': 'off',
+    'no-use-before-define': 'off',
+    'no-console': 'off',
+    'no-restricted-syntax': 'off',
+    'max-len': 'off'
+  },
+}
